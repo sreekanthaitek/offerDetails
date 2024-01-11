4f4f97d14e1588a7abc0f37b6ef7917ceb832e61
sreekanthaitek <153638256+sreekanthaitek@users.noreply.github.com>
2024-01-11 18:35:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ app.get('/upload',(req,res)=>{
   res.sendFile(__dirname+'/public/offerForm.html');
 });
 
-app.post('/update',authenticate async (req, res) => {
+app.post('/update',authenticate, async (req, res) => {
   let lName = req.body.lName1;
     let {fName,nationality,birthPlace,passNumber,pid,ped,pic,dob,gender,race,religion,ms,homeAdd,spouse,child,mail,tCon,mCon,bankDetails,pBank} = req.body;
     let {addrBank,accNum,sortCode,bLocation,swiftCode,iban,bInfo,taxIdentity,emgName,emgRelation,contact,addr} = req.body;
